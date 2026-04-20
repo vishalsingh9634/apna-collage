147293c3bd8072fd0b0e3710f05fd1ca16b5326e
Vishal Singh <vishalsinghbrn@gmail.com>
2026-04-21 02:51:35 +0530

    linear_search

---
+++ function program/linear_search.java
@@ -0,0 +1,15 @@
+public class linear_search {
+    public static int linear(int num[],int target){
+        for(int i=0;i<=num.length-1;i++){
+            if(target==num[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int num[]={10,20,30,40,50,60,70,80};
+        int target=20;
+        System.out.println(linear(num, target));
+    }
+}
