7482f964fd13a38faf23253686170b18722bed1a
Vishal Singh <vishalsinghbrn@gmail.com>
2026-04-28 17:13:28 +0530

    trapped water

---
+++ function program/trapping_water.java
@@ -0,0 +1,26 @@
+public class trapping_water {
+    public static int trapped_water(int height[]){
+        int n=height.length;
+        int leftmax[]=new int[n];
+        leftmax[0]=height[0];
+        for(int i=1;i<n;i++){
+            leftmax[i]=Math.max(height[i],leftmax[i-1]);
+        }
+        int rightmax[]=new int[n];
+        rightmax[n-1]=height[n-1];
+        for(int i=n-2;i>=0;i--){
+            rightmax[i]=Math.max(height[i],rightmax[i+1]);
+        }
+        int trappedwater=0;
+
+        for(int i=0;i<n;i++){
+            int waterlavel=Math.min(leftmax[i],rightmax[i]);
+            trappedwater+=waterlavel-height[i];
+        }
+        return trappedwater; 
+    }
+    public static void main(String[] args) {
+        int height[]={4,2,0,6,3,2,5};
+        System.out.println(trapped_water(height));
+    }
+}
