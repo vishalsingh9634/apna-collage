9ab86a68fee1c77d605b983dba20f6e6bf789367
Vishal Singh <vishalsinghbrn@gmail.com>
2026-04-28 12:16:22 +0530

    reverse array

---
+++ function program/reverse_array.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class reverse_array {
+    public static void reverse(int arr []){
+        int start=0;
+        int end=arr.length-1;       
+        while(start<end){
+            int temp=arr[end];
+                arr[end]=arr[start];
+                arr[start]=temp;
+                start++;
+                end--;
+        }      
+    }
+    public static void main(String[] args) {
+        int arr[]={10,20,30,50,40};
+        reverse(arr);
+        for(int i=0;i<arr.length;i++){
+        System.out.print(arr[i]+" ");
+    }
+    }
+}
