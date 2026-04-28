19db244233bc5a7187c242f84b786ac209860932
Vishal Singh <vishalsinghbrn@gmail.com>
2026-04-28 13:45:49 +0530

    pair of array

---
+++ function program/pairarrya.java
@@ -0,0 +1,17 @@
+public class pairarrya {
+    public static void pair(int num[]){
+        int tp=0;
+        for(int i=0;i<num.length;i++){
+            for(int j=i+1;j<num.length;j++){
+                System.out.print("(" +num[i]+","+num[j]+")");
+                tp++;
+            }
+            System.out.println();            
+        }
+        System.out.println("total pair:"+tp);
+    }
+    public static void main(String[] args) {
+        int num[]={10,20,30,40,50};
+        pair(num);
+    }
+}
