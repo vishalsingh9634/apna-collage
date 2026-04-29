43fc26cdca7dd2b3bbb9f5b6751769771fc5ba14
Vishal Singh <vishalsinghbrn@gmail.com>
2026-04-29 18:28:20 +0530

    selecttion sort

---
+++ sorting/selection_sort.java
@@ -0,0 +1,23 @@
+public class selection_sort {
+    public static void selection(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
+            int min=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[min]>arr[j]){
+                    min=j;
+                }
+            }
+            int temp=arr[min];
+            arr[min]=arr[i];
+            arr[i]=temp;
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,5,7,8,3};
+        selection(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+}
