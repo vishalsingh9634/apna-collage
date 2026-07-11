488867626b60b8f8dace4dfbe188386bbd6c17d2
Vishal Singh <vishalsinghbrn@gmail.com>
2026-07-11 18:59:21 +0530

    oopsfirstprogram

--- oops/classandobj.java
+++ oops/classandobj.java
@@ -1,6 +1,20 @@
-package oops;
-// objects = entities in the real world
-// classes = group of these entities
 public class classandobj {
-    
+    public static void main(String[] args) {
+    pen p=new pen();
+    p.setcolour("blue");
+    p.setprice(20);
+    System.out.println(p.colour);
+    System.out.println(p.price);
+    }
+}
+
+class pen{
+    String colour;
+    int price;
+    void setcolour(String newcolour){
+        colour=newcolour;
+    }
+    void setprice(int newprice){
+        price =newprice;
+    }
 }
